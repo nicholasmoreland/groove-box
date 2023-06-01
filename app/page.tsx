e8503aea417d236e66ac50fc80d06735abb0c203
nicholasmoreland <nmoreland18@outlook.com>
2023-05-31 23:25:31 -0500

    update: added verification banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,18 @@ export default function Home() {
     <div className={theme === "dark" ? "dark" : undefined}>
       <main className="bg-white dark:bg-gray-900 min-h-screen">
         <NavBar />
+
+        {user?.emailVerified ? undefined : (
+          <div className="mt-4 mx-4">
+            <div className="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative lg:mx-16">
+              <strong className="font-bold">Alert! </strong>
+              <span className="block sm:inline">
+                Check email to verify your account. Features will be limited
+                until you verify your account.
+              </span>
+            </div>
+          </div>
+        )}
       </main>
     </div>
   );
